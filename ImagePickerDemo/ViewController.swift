cea8296a8ce1163fd018da093c6c7ab14608d8e0
Vấn Lê <vanlh@itpsolution.net>
2019-10-08 17:52:06 +0700

    upload demo project

---
+++ ImagePickerDemo/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ImagePickerDemo
+//
+//  Created by Van Le on 10/8/19.
+//  Copyright © 2019 ITPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+    }
+    
+    @IBAction func didTapAddButton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Photo Gallery", style: .default, handler: { (button) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (button) in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        imageView.image = pickedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
